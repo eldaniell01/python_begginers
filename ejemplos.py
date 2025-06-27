cd9a245dcce59efad7e984dbff0066715a380adb
eldaniell01 <amontepequem@miumg.edu.gt>
2025-06-26 21:25:12 -0600

    inicio

---
+++ ejemplos.py
@@ -0,0 +1,9 @@
+"""
+    ingreso de multiples valores
+
+"""
+a, b, c = input("dime la a, la b y c\n").split()
+
+print(a)
+print(b)
+print(c)
---
+++ ejercicios.py
@@ -0,0 +1,59 @@
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones.
+###
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+### Completa aquí
+
+print("mi nombre es daniel \n soy de guatemala")
+
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+### Completa aquí
+
+print(type(a), type(b), type(c), type(d),type(e))
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+
+print(float("12345"))
+print(int(3.99))
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+### Completa aquí
+name = "daniel"
+age = 15
+tall = 1.6
+
+print(f'hola me llamo {name}, y tengo {age} y mido {tall}')
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
+
+variable = int(round(3.1416)/2)
+print("valor de pi= 3.1416")
+print("valor redondeado ", round(3.1416))
+print(f'el resultado es {variable}')
